4a86098cbf560b08e355a16fe64f21c2ba011281
fusarocr <fusarocr@rose-hulman.edu>
2017-12-21 00:53:55 -0500

    start with str(m)

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -144,10 +144,11 @@ def make_simple_string(m, n):
 
     return string
 
+
 def run_test_make_less_simple_string():
     """ Tests the   make_less_simple_string    function. """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  make_less_simple_string  function defined below.
     #   Include at least **   2   ** tests.
     #
@@ -157,6 +158,18 @@ def run_test_make_less_simple_string():
     print('--------------------------------------------------')
     print('Testing the   make_less_simple_string   function:')
     print('--------------------------------------------------')
+
+    # Test 1:
+    expected = '5-6-7-8-9-10-11-12-13'
+    actual = make_less_simple_string(5, 13)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 2:
+    expected = '205'
+    actual = make_less_simple_string(205, 205)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 
 def make_less_simple_string(m, n):
@@ -182,10 +195,15 @@ def make_less_simple_string(m, n):
       :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -----------------------------------------------------------------
 
+    seq = str(m)
+    for k in range(m+1, n+1):
+       seq = seq + '-'+ str(k)
+
+    return seq
 
 def run_test_draw_shapes():
     """ Tests the   draw_shapes    function. """
